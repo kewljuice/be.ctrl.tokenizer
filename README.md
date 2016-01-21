f2d7194487a89dbdbf65e65cbbd39ce40bb3e34a
kewljuice <kewljuice@hotmail.com>
2016-01-21 15:01:53 +0100

    Initial commit
    Created files for extension

---
+++ README.md
@@ -0,0 +1,24 @@
+# CiviCRM extension: Tokenizer
+
+Tokenizer: Custom tokens for CiviCRM.
+- [Installation](#installation)
+
+***
+
+## Installation
+
+- You can directly clone to your CiviCRM extension directory using<br>
+```$ git clone https://github.com/kewljuice/be.ctrl.tokenizer.git```
+
+- You can also download a zip file, and extract in your extension directory<br>
+```$ git clone https://github.com/kewljuice/be.ctrl.tokenizer/archive/master.zip```
+
+- Configure CiviCRM Extensions Directory which can be done from<br>
+```"Administer -> System Settings -> Directories".```
+
+- Configure Extension Resource URL which can be done from<br>
+```"Administer -> System Settings -> Resource URLs".```
+
+- The next step is enabling the extension which can be done from<br> 
+```"Administer -> System Settings -> Manage CiviCRM Extensions".```
+
---
+++ tokenizer.php
@@ -0,0 +1,127 @@
+<?php
+
+require_once 'nsynch.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function nsynch_civicrm_config(&$config) {
+  _nsynch_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function nsynch_civicrm_xmlMenu(&$files) {
+  _nsynch_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function nsynch_civicrm_install() {
+  _nsynch_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function nsynch_civicrm_uninstall() {
+  _nsynch_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function nsynch_civicrm_enable() {
+	// continue
+  _nsynch_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function nsynch_civicrm_disable() {
+  // continue
+	_nsynch_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function nsynch_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _nsynch_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function nsynch_civicrm_managed(&$entities) {
+  _nsynch_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function nsynch_civicrm_caseTypes(&$caseTypes) {
+  _nsynch_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function nsynch_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _nsynch_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * buildform
+ */
+function nsynch_civicrm_buildForm($formName, &$form) {
+	// set buildForm
+}
+
+/**
+ * pagerun
+ */
+function nsynch_civicrm_pageRun( &$page ) {
+	// set pageRun
+}
+
+
+ 
